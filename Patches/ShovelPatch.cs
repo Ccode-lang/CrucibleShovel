76f9eef64ea5bfe04abdd742ba564803426f2ad8
Ccode-lang <coop112097@gmail.com>
2024-02-01 10:31:57 -0700

    Add project files.

---
+++ Patches/ShovelPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CrucibleShovel.Patches
+{
+    [HarmonyPatch(typeof(GrabbableObject), "Start")]
+    internal class ShovelPatch
+    {
+        public static void Prefix(GrabbableObject __instance)
+        {
+            if (__instance.GetType() == typeof(Shovel))
+            {
+                MonoBehaviour bh = __instance as MonoBehaviour;
+                bh.gameObject.GetComponent<MeshFilter>().sharedMesh = CrucibleShovel.Plugin.newModel;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace CrucibleShovel
+{
+    [BepInPlugin(MyGuid, PluginName, VersionString)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string MyGuid = "Ccode.Crucible";
+        private const string PluginName = "CrucibleShovel";
+        private const string VersionString = "1.5";
+
+        private static readonly Harmony Harmony = new Harmony(MyGuid);
+
+        public static ManualLogSource Log;
+
+        public static Mesh newModel;
+        public void Awake()
+        {
+            string location = ((BaseUnityPlugin)this).Info.Location;
+            string text = "CrucibleShovel.dll";
+            string text2 = location.TrimEnd(text.ToCharArray());
+            string text3 = text2 + "crucible";
+            AssetBundle val = AssetBundle.LoadFromFile(text3);
+            newModel = val.LoadAssetWithSubAssets<Mesh>("untitled.obj")[0];
+            Harmony.PatchAll();
+            Logger.LogInfo(PluginName + " " + VersionString + " " + "loaded.");
+            Log = Logger;
+        }
+    }
+}
